146b0999daf797a66202d3680abe0b820eeefab1
lang-sky <skylang121@outlook.com>
2020-01-23 05:09:58 +0800

    'subscription'

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { AUTH_TOKEN } from './constants';
 
-// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
@@ -23,16 +25,33 @@ const authLink = setContext((_, { headers }) => {
       ...headers,
       authorization: token ? `Bearer ${token}` : ''
     }
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
   }
 });
 
-// 3
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
-// 4
 ReactDOM.render(
   <Router>
     <ApolloProvider client={client}>
